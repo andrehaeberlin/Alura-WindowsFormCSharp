2c0b1853878e00770052f1c06759658d37936dc3
Andre Luis Haeberlin <andrehaeberlin@outlook.com>
2021-05-18 22:25:41 -0300

    3 aplicando o menu flutuante na aplicacao

--- CursoWindowsForms/Frm_Principal_Menu_UC.Designer.cs
+++ CursoWindowsForms/Frm_Principal_Menu_UC.Designer.cs
@@ -32,6 +32,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Principal_Menu_UC));
             this.Mnu_Principal = new System.Windows.Forms.MenuStrip();
             this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.conectarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desconectarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.demonstraçãoKeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helloWordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -46,8 +48,6 @@
             this.windowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.Tbc_Principal = new System.Windows.Forms.TabControl();
             this.Iml_Principal = new System.Windows.Forms.ImageList(this.components);
-            this.desconectarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.conectarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.Mnu_Principal.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -74,6 +74,20 @@
             this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
             this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
             this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // conectarToolStripMenuItem
+            // 
+            this.conectarToolStripMenuItem.Name = "conectarToolStripMenuItem";
+            this.conectarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.conectarToolStripMenuItem.Text = "Conectar";
+            this.conectarToolStripMenuItem.Click += new System.EventHandler(this.conectarToolStripMenuItem_Click);
+            // 
+            // desconectarToolStripMenuItem
+            // 
+            this.desconectarToolStripMenuItem.Name = "desconectarToolStripMenuItem";
+            this.desconectarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.desconectarToolStripMenuItem.Text = "Desconectar";
+            this.desconectarToolStripMenuItem.Click += new System.EventHandler(this.desconectarToolStripMenuItem_Click);
             // 
             // novoToolStripMenuItem
             // 
@@ -186,6 +200,7 @@
             this.Tbc_Principal.SelectedIndex = 0;
             this.Tbc_Principal.Size = new System.Drawing.Size(800, 426);
             this.Tbc_Principal.TabIndex = 8;
+            this.Tbc_Principal.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Tbc_Principal_MouseDown);
             // 
             // Iml_Principal
             // 
@@ -198,20 +213,6 @@
             this.Iml_Principal.Images.SetKeyName(4, "Frm_ValidaCPF2.png");
             this.Iml_Principal.Images.SetKeyName(5, "Frm_ValidaSenha.png");
             this.Iml_Principal.Images.SetKeyName(6, "Folder_Icon.png");
-            // 
-            // desconectarToolStripMenuItem
-            // 
-            this.desconectarToolStripMenuItem.Name = "desconectarToolStripMenuItem";
-            this.desconectarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.desconectarToolStripMenuItem.Text = "Desconectar";
-            this.desconectarToolStripMenuItem.Click += new System.EventHandler(this.desconectarToolStripMenuItem_Click);
-            // 
-            // conectarToolStripMenuItem
-            // 
-            this.conectarToolStripMenuItem.Name = "conectarToolStripMenuItem";
-            this.conectarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.conectarToolStripMenuItem.Text = "Conectar";
-            this.conectarToolStripMenuItem.Click += new System.EventHandler(this.conectarToolStripMenuItem_Click);
             // 
             // Frm_Principal_Menu_UC
             // 
--- CursoWindowsForms/Frm_Principal_Menu_UC.cs
+++ CursoWindowsForms/Frm_Principal_Menu_UC.cs
@@ -163,7 +163,7 @@ namespace CursoWindowsForms
                     abrirImagemToolStripMenuItem.Enabled = true;
                     conectarToolStripMenuItem.Enabled = false;
                     desconectarToolStripMenuItem.Enabled = true;
-                    
+
 
                     MessageBox.Show("Bem vindo " + login + " !", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
@@ -183,7 +183,7 @@ namespace CursoWindowsForms
 
             if (Db.DialogResult == DialogResult.Yes)
             {
-                for (int i = Tbc_Principal.TabPages.Count -1; i >= 0; i+= -1)
+                for (int i = Tbc_Principal.TabPages.Count - 1; i >= 0; i += -1)
                 {
                     Tbc_Principal.TabPages.Remove(Tbc_Principal.TabPages[i]);
                 }
@@ -196,6 +196,89 @@ namespace CursoWindowsForms
             }
 
         }
+
+        private void Tbc_Principal_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                var ContextMenu = new ContextMenuStrip();
+                var vToolTip001 = DesenhaItemMenu("Fechar a aba", "DeleteTab");
+                var vToolTip002 = DesenhaItemMenu("Fechar todas abas à esquerda", "DeleteLeft");
+                var vToolTip003 = DesenhaItemMenu("Fechar todas abas à direita", "DeleteRight");
+                var vToolTip004 = DesenhaItemMenu("Fechar todas abas, menos esta", "DeleteAll");
+                ContextMenu.Items.Add(vToolTip001);
+                ContextMenu.Items.Add(vToolTip002);
+                ContextMenu.Items.Add(vToolTip003);
+                ContextMenu.Items.Add(vToolTip004);
+                ContextMenu.Show(this, new Point(e.X, e.Y));
+                vToolTip001.Click += new System.EventHandler(vToolTip001_Click);
+                vToolTip002.Click += new System.EventHandler(vToolTip002_Click);
+                vToolTip003.Click += new System.EventHandler(vToolTip003_Click);
+                vToolTip004.Click += new System.EventHandler(vToolTip004_Click);
+            }
+        }
+        void vToolTip001_Click(object sender1, EventArgs e1)
+        {
+            if (Tbc_Principal.SelectedTab != null)
+            {
+                Tbc_Principal.TabPages.Remove(Tbc_Principal.SelectedTab);
+                MessageBox.Show("Esta aba foi fechada");
+            }
+
+        }
+        void vToolTip002_Click(object sender1, EventArgs e1)
+        {
+            if (Tbc_Principal.SelectedTab != null)
+            {
+                ApagaEsquerda(Tbc_Principal.SelectedIndex);
+                MessageBox.Show("Todas as abas a esquerda foram fechadas!");
+            }
+        }
+        void vToolTip003_Click(object sender1, EventArgs e1)
+        {
+            if (Tbc_Principal.SelectedTab != null)
+            {
+                ApagaDireita(Tbc_Principal.SelectedIndex);
+                MessageBox.Show("Todas as abas a direita foram fechadas!");
+            }
+        }
+
+        void vToolTip004_Click(object sender1, EventArgs e1)
+        {
+            if (Tbc_Principal.SelectedTab != null)
+            {
+                ApagaEsquerda(Tbc_Principal.SelectedIndex);
+                ApagaDireita(Tbc_Principal.SelectedIndex);
+                MessageBox.Show("Todas abas fechadas menos esta!");
+            }
+
+        }
+        ToolStripMenuItem DesenhaItemMenu(string text, string nomeImagem)
+        {
+            var vToolTip = new ToolStripMenuItem();
+            vToolTip.Text = text;
+            Image MyImage = (Image)global::CursoWindowsForms.Properties.Resources.ResourceManager.GetObject(nomeImagem);
+            vToolTip.Image = MyImage;
+            return vToolTip;
+        }
+
+        void ApagaEsquerda(int ItemSelecionado)
+        {
+            for (int i = ItemSelecionado - 1; i >= 0; i += -1)
+            {
+                Tbc_Principal.TabPages.Remove(Tbc_Principal.TabPages[i]);
+            }
+        }
+
+        void ApagaDireita(int ItemSelecionado)
+        {
+            for (int i = Tbc_Principal.TabCount - 1; i > ItemSelecionado; i += -1)
+            {
+                Tbc_Principal.TabPages.Remove(Tbc_Principal.TabPages[i]);
+            }
+        }
     }
 }
+
+
 
