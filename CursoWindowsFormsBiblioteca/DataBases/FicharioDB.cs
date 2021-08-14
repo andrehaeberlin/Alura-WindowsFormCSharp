d8508fb69977c295c22cec46f7427f288575960d
Andre Luis Haeberlin <andrehaeberlin@outlook.com>
2021-08-14 00:47:54 -0300

    Falta fechar o CRUD

---
+++ CursoWindowsFormsBiblioteca/DataBases/FicharioDB.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class FicharioDB
+    {
+
+        public string mensagem;
+        public bool status;
+        public string tabela;
+        public LocalDBClass db;
+
+        public FicharioDB(string Tabela)
+        {
+            status = true;
+            try
+            {
+                db = new LocalDBClass();
+                tabela = Tabela;
+                mensagem = "Conexão com a Tabela criada com sucesso.";
+
+            }
+            catch (Exception ex)
+            {
+
+                status = false;
+                mensagem = "Conexão com a Tabela com erro: " + ex.Message;
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Incluir(string Id, string jsonUnit)
+        {
+            status = true;
+            try
+            {
+
+                // INSERT INTO CLIENTE (ID, JSON) VALUES ('000001', '{...}')
+
+                var SQL = "INSERT INTO" + tabela + "(Id, JSON) VALUES('" + Id + "', '" + jsonUnit + "')";
+                db.SQLCommand(SQL);
+                mensagem = "Inclusão efetuada com sucesso.";
+
+            }
+            catch (Exception ex)
+            {
+
+                status = false;
+                mensagem = "Conexão com a Tabela com erro: " + ex.Message;
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public string Buscar(string Id)
+        {
+            status = true;
+            try
+            {
+
+                // SELECT ID, JSON FROM CLIENTE WHERE ID =  '000001'
+                var SQL = "SELECT Id, JSON FROM " + tabela + " WHERE ID = '" + Id + "'";
+                var dt = db.SQLQuery(SQL);
+                if (dt.Rows.Count > 0)
+                {
+                    string conteudo = dt.Rows[0]["JSON"].ToString();
+                    status = true;
+                    mensagem = "Busca efetuada com sucesso. Identificador: " + Id;
+                    return conteudo;
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Identificador não existente: " + Id;
+                    return string.Empty;
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                status = false;
+                mensagem = "Conexão com a Tabela com erro: " + ex.Message;
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<string> BuscarTodos()
+        {
+            status = true;
+            List<string> List = new List<string>();
+
+            try
+            {
+                // SELECT ID, JSON FROM CLIENTE WHERE ID =  '000001'
+                var SQL = "SELECT Id, JSON FROM " + tabela;
+                var dt = db.SQLQuery(SQL);
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i <= dt.Rows.Count - 1; i++)
+                    {
+                        string conteudo = dt.Rows[i]["JSON"].ToString();
+                        List.Add(conteudo);
+                    }                    
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Usuários não encontrados";
+                    List =  null;
+                }
+
+                return List;
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Conexão com a Tabela com erro: " + ex.Message;
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Apagar(string Id)
+        {
+            status = true;
+            try
+            {
+
+                // SELECT ID, JSON FROM CLIENTE WHERE ID =  '000001'
+                var SQL = "SELECT Id, JSON FROM " + tabela + " WHERE ID = '" + Id + "'";
+                var dt = db.SQLQuery(SQL);
+                if (dt.Rows.Count > 0)
+                {
+                    //DELETE FROM CLIENTE WHERE ID =  '000001'
+                    SQL = "DELETE FROM " + tabela + " WHERE ID = '" + Id + "'";                    
+                    status = true;
+                    mensagem = "Usuário apagado com sucesso. Identificador: " + Id;                    
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Identificador não existente: " + Id;                    
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                status = false;
+                mensagem = "Conexão com a Tabela com erro: " + ex.Message;
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Alterar(string Id, string jsonUnit)
+        {
+
+            status = true;
+            try
+            {
+
+                // SELECT ID, JSON FROM CLIENTE WHERE ID =  '000001'
+                var SQL = "SELECT Id, JSON FROM " + tabela + " WHERE ID = '" + Id + "'";
+                var dt = db.SQLQuery(SQL);
+                if (dt.Rows.Count > 0)
+                {
+                    //UPDATE CLIENTE SET JSON = '{...}' WHERE ID =  '000001'
+                    SQL = "UPDATE " + tabela + " SET JSON = '" + jsonUnit + "' WHERE ID = '" + Id + "'";
+                    status = true;
+                    mensagem = "Usuário alterado com sucesso. Identificador: " + Id;
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Identificador não existente: " + Id;
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                status = false;
+                mensagem = "Conexão com a Tabela com erro: " + ex.Message;
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+
+}
---
+++ CursoWindowsFormsBiblioteca/DataBases/LocalDBClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class LocalDBClass
+    {
+
+        public string stringConn;
+        public SqlConnection connDB;
+
+        public LocalDBClass()
+        {
+            try
+            {
+
+                stringConn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\andre\\Source\\Repos\\andrehaeberlin\\Alura-WindowsFormCSharp\\CursoWindowsFormsBiblioteca\\Databases\\Fichario.mdf;Integrated Security=True";
+                connDB = new SqlConnection(stringConn);
+                connDB.Open ();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public DataTable SQLQuery(string SQL)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+
+                SqlCommand myCommand = new SqlCommand(SQL, connDB);   
+                myCommand.CommandTimeout = 0;
+                var myReader = myCommand.ExecuteReader();
+                dt.Load(myReader); 
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+            return dt;  
+        }
+
+        public string SQLCommand (string SQL)
+        {
+            try
+            {
+
+                var myCommand = new SqlCommand(SQL, connDB);
+                myCommand.CommandTimeout = 0;
+                var myReader = myCommand.ExecuteReader();
+                return "";
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Close()
+        {
+            connDB.Close();
+        }
+    }
+}
