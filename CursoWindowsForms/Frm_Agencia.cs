89507a5d4a85f0d21ff9b66ebc3bde07a128fe3c
Andre Luis Haeberlin <andrehaeberlin@outlook.com>
2021-09-23 21:45:04 -0300

    saving

--- CursoWindowsForms/Frm_Agencia.cs
+++ CursoWindowsForms/Frm_Agencia.cs
@@ -16,7 +16,22 @@ namespace CursoWindowsForms
         {
             InitializeComponent();
             this.Text = "Cadastro de Agência";
-            Tls_Principal.Items[0].ToolTipText = "Fechar a caixa de diálogo";
+            Tls_Agencia.Items[0].ToolTipText = "Fechar a caixa de diálogo";
+        }
+
+        private void tB_AgenciaBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tB_AgenciaBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.byteBankDataSet);
+
+        }
+
+        private void Frm_Agencia_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'byteBankDataSet.TB_Agencia' table. You can move, or remove it, as needed.
+            this.tB_AgenciaTableAdapter.Fill(this.byteBankDataSet.TB_Agencia);
+
         }
     }
 }
